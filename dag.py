a05db8a5866d583c897d962d6d681b80a05540d5
BogGoro <151636316+BogGoro@users.noreply.github.com>
2024-09-18 20:40:01 +0300

    add daily_report

--- dag.py
+++ dag.py
@@ -1,23 +1,54 @@
-import time
-import pandas as pd
-
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType
+from pyspark.sql.functions import col, count, when
+
+schema = StructType([
+    StructField("email", StringType(), False),
+    StructField("action", StringType(), False),
+    StructField("date", TimestampType(), False)
+])
 
 def skip_if_catchup(**kwargs):
     if kwargs['execution_date'] < days_ago(0):
         return 'skip_weekly_report'
     return 'create_weekly_report'
 
-def create_dayly_report(date):
-    return 
+def create_daily_report(date):
+    spark = SparkSession.builder \
+        .appName("Create daily report") \
+        .getOrCreate()
+
+    csv_file_path = f"/input/{date}.csv"
+
+    df = spark.read.csv(
+        csv_file_path,
+        schema=schema,
+        header=False,
+        sep=","
+    )
+
+    counted_actions = df.groupBy("email").agg(
+        count(when(col("action") == "create", 1)).alias("create_num"),
+        count(when(col("action") == "read", 1)).alias("read_num"),
+        count(when(col("action") == "update", 1)).alias("update_num"),
+        count(when(col("action") == "delete", 1)).alias("delete_num")
+    )
+
+    output_path = f"/daily_reports/{date}.csv"
+
+    counted_actions.write.csv(output_path, header=True, mode='overwrite')
+
+    spark.stop()
+    return 200
 
 def create_weekly_report(date):
-    return 
+    return 200
 
 args = {
     "owner": "BogGoro",
@@ -47,19 +78,19 @@ with DAG(
         )
     )
 
-    create_dayly_report = PythonOperator(
-        task_id='create_dayly_report',
-        python_callable=create_dayly_report,
+    create_daily_report_task = PythonOperator(
+        task_id='create_daily_report',
+        python_callable=create_daily_report,
         op_kwargs={'date': business_dt}
     )
     
-    skip_if_catchup = BranchPythonOperator(
+    skip_if_catchup_task = BranchPythonOperator(
         task_id='skip_if_catchup',
         python_callable=skip_if_catchup,
         provide_context=True
     )
 
-    create_weekly_report = PythonOperator(
+    create_weekly_report_task = PythonOperator(
         task_id='create_weekly_report',
         python_callable=create_weekly_report,
         op_kwargs={'date': business_dt}
@@ -72,8 +103,8 @@ with DAG(
     (
         start
         >> get_todays_file
-        >> create_dayly_report
-        >> skip_if_catchup
-        >> [create_weekly_report, skip_weekly_report]
+        >> create_daily_report_task
+        >> skip_if_catchup_task
+        >> [create_weekly_report_task, skip_weekly_report]
         >> end
     )
